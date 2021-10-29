ca7cb9c7b1adf650c5582d9bcdf42292340113cb
Ryan Shane Palm <rspalm@bigpond.com>
2021-10-29 11:29:09 +0200

    Setup of public dir

--- public/index.php
+++ public/index.php
@@ -2,5 +2,4 @@
 
 require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-
 echo "hello world!!!";
--- src/Install/Script/InstallScript.php
+++ src/Install/Script/InstallScript.php
@@ -2,11 +2,39 @@
 
 namespace Palmyr\WebApp\Install\Script;
 
+use Composer\Script\Event;
+
 class InstallScript
 {
 
-    static public function install(): void
+    static public function install(Event $event): void
     {
-        echo "hello world!!!" . PHP_EOL;
+        $rootDir = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+        static::setupPublicDir($rootDir);
     }
+
+    static public function update(Event $event): void
+    {
+        $rootDir = dirname($event->getComposer()->getConfig()->get('vendor-dir'));
+        static::setupPublicDir($rootDir);
+    }
+
+    static protected function setupPublicDir(string $rootDir): void
+    {
+        $publicDir = $rootDir . DIRECTORY_SEPARATOR . 'public';
+        $indexTemplate = dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . 'index.php.txt';
+
+        if ( !is_dir($publicDir) ) {
+            if ( !mkdir($publicDir) ) {
+                throw new \RuntimeException('Failed to create public directory');
+            }
+        }
+
+
+        if ( !copy($indexTemplate, $publicDir . DIRECTORY_SEPARATOR . 'index.php') ) {
+            throw new \RuntimeException('Failed to create index.php');
+        }
+    }
+
+
 }
